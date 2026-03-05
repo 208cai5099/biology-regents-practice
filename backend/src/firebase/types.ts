422adcba356c3ebe00e7dabb98f5c0b30449e192
208cai5099 <208cai5099@gmail.com>
2026-03-05 00:33:32 -0500

    Uploaded curriculum materials to Firebase

---
+++ backend/src/firebase/types.ts
@@ -0,0 +1,26 @@
+export interface ScienceStandard {
+    topic: string,
+    standard_code: string,
+    performance_expectation: string,
+    clarification_statement: string
+}
+
+export interface Question {
+    number: number,
+    question: string,
+    answer: string,
+    standard: string
+}
+
+export interface Cluster {
+    source_name: string,
+    title: string,
+    content: string,
+    questions: Question[],
+    phenomenon: string[]
+}
+
+export interface PhenomenaList {
+    already_used: string[],
+    new_phenomena: string[]
+}
